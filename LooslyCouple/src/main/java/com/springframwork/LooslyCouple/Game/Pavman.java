2cacef1d779da428a4f2f6135e2eea1fc8afc9e8
Rutuja <patilrutuja9168@gmail.com>
2024-03-31 10:09:30 +0530

    loose couple

---
+++ LooslyCouple/src/main/java/com/springframwork/LooslyCouple/Game/Pavman.java
@@ -0,0 +1,37 @@
+package com.springframwork.LooslyCouple.Game;
+
+public class Pavman implements GameInter {
+
+	@Override
+	public void left() {
+		// TODO Auto-generated method stub
+		System.out.println("pavman :: left");
+
+		
+	}
+
+	@Override
+	public void right() {
+		// TODO Auto-generated method stub
+		System.out.println("pavman :: right");
+
+		
+	}
+
+	@Override
+	public void top() {
+		// TODO Auto-generated method stub
+		System.out.println("pavman :: top");
+
+		
+	}
+
+	@Override
+	public void down() {
+		// TODO Auto-generated method stub
+		System.out.println("pavman :: down");
+
+		
+	}
+
+}
---
+++ LooslyCouple/src/test/java/com/springframwork/LooslyCouple/LooslyCoupleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springframwork.LooslyCouple;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LooslyCoupleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
